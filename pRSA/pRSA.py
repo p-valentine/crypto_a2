b16df3bf17927cb55f9e6e573fb2f09df6b5e2e5
peter valentine <valentpj@clarkson.edu>
2019-12-14 16:45:29 -0500

    added stuff

--- pRSA/pRSA.py
+++ pRSA/pRSA.py
@@ -11,7 +11,7 @@ def isPrime(n, k):
 
         while d % 2 == 0:
             s = s + 1
-            d = int(d / 2)
+            d >>= 1
 
         for i in range(k):
             a = random.randint(2, n - 2)
@@ -48,25 +48,46 @@ def generate_e_d():
     pass
 
 
-def generate_p_q():
-    pass
+def generate_p_q(k):
+    result = False
+    p, q = None, None
+    while p is None:
+        p = random.randint(2 ** (k - 1), (2 ** k) - 1)
+        if p % 2 != 0:
+            result = isPrime(p, 30)
+            print("done testing isPrime")
+        if result is True:
+            print("P is prime")
+        else:
+            p = None
+            print("p is not prime :( ", p)
+    while q is None and q != p:
+        q = random.randint(2 ** (k - 1), (2 ** k) - 1)
+        if q % 2 != 0:
+            result = isPrime(q, 30)
+            print("done testing isPrime")
+        if result is True:
+            print("q is prime")
+        else:
+            q = None
+            print("q is not prime :( ", q)
+
+    return p, q
 
 
 def compute_n_phi(p, q):
-    pass
+    n = p * q
+    phi = (p - 1) * (q - 1)
+
+    return n, phi
 
 
 def main():
     k = getK()
-    is_odd = False
-    while is_odd is False:
-        r = random.randint(2 ** (k - 1), (2 ** k) - 1)  # generates random k bit number
-        # print(r)
-        if r % 2 != 0:
-            is_odd = True
-    result = isPrime(31, 100)
-
-    print("result", result)
+    p, q = generate_p_q(k)
+    print("P= ", p, " Q= ", q)
+    n, phi = compute_n_phi(p, q)
+    print("N= ", n, "Phi= ", phi) 
 
 
 if __name__ == "__main__":
